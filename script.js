709524864eb6cf4d151719a09d5f462031dc84b1
damien <d.macors@gmail.com>
2019-09-11 15:08:53 +0200

    fin et 12

--- script.js
+++ script.js
@@ -1,10 +1,48 @@
 (() => {
     let mydiv = document.getElementById("affichage");
     let mybtn = document.getElementById("click");
+    let btnmulti = document.getElementById("multiplier");
+    let btnautoclick = document.getElementById("autoclick");
+    let multiplicateur = 1;
+    let score = 0;
+    let cout = 50;
+    btnmulti.innerText = `Multiplicateur x${multiplicateur} prochain achat :${cout}`;
+
+    function augmenterMultiplicateur(elem) {
+        cout = cout * 2;
+        multiplicateur++;
+        elem.innerText = `Multiplicateur x${multiplicateur} prochain achat :${cout}`;
+    }
 
     mybtn.addEventListener("click", () => {
-        let inc = parseInt(mydiv.innerText);
-        inc++;
-        mydiv.innerText = inc;
+        increment();
     });
+
+    btnmulti.addEventListener("click", (e) => {
+        if (score >= cout) {
+            score = score - cout;
+            augmenterMultiplicateur(e.target);
+            mydiv.innerText = score;
+        } else
+            alert("la maison ne fait pas crédit !!");
+
+    });
+
+    btnautoclick.addEventListener("click", (e) => {
+        if (score >= 500) {
+            setInterval(() => {
+                increment();
+            }, 1000);
+            score = score - 500;
+            mydiv.innerText = score;
+            e.target.setAttribute("disabled", "true");
+        }
+    });
+
+    function increment() {
+        let inc = parseInt(mydiv.innerText);
+        score = inc + multiplicateur;
+        mydiv.innerText = score;
+        return score;
+    }
 })();
